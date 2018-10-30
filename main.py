0e1068e65ea48b6e74452b2760c8427c35cc6fc6
MrAngle <mr.angle69@gmail.com>
2018-10-30 01:29:29 +0100

    add TODO info

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 from Configuration import *
 from htmlResourceParser import *
 
+#TODO links = [STARTING_PAGE]
 
-
-
+#TODO: for nextLink in links
 tree = PageResource(STARTING_PAGE, DICTIONARY)
 print("PARAGRAPHS === Liczba elementow (PARAGRAPHS) {}".format(len(tree.data[PageResource.PARAGRAPHS])))
 print(tree.data[PageResource.PARAGRAPHS])
@@ -23,6 +23,11 @@ print(tree.data[PageResource.DIVS])
 print("\n")
 
 tree.saveWordsToFile()
+#TODO: links.append(tree.links)
+
+#TODO: DODAC ZABEZPIECZENIE PRZED POBIERANIEM DANYCH Z TEJ SAMEJ STRONY (NP. plik w którym bedzie lista u¿ytych stron)
+#TODO: STWORZYC LISTE Z LINKAMI - dodawac do niej caly czas nowe linki wraz z wchodzeniem "glebiej" w inne podstrony
+#TODO: STWORZYC PETLE KTORA BEDZIE WYKONWYAC TO SAMO CO POWYZEJ AZ DO OSIAGNIECIA PEWNEJ LICZBY PLIKOW | ALTERNATYWNIE: JAKIS WATEK SPRAWDZAJACY CZAS
 
 
 
